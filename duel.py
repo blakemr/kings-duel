54c552ba05feff1f4db3a4270750c7376c93cf53
Matthew Blake <mblak0625@gmail.com>
2021-02-06 12:21:42 -0500

    changed it so the engine path is set as an argument

--- duel.py
+++ duel.py
@@ -1,5 +1,8 @@
 # duel.py - run a simulation of a chess tournaments and output the results
+#
+# Takes in an argument for the path of the engine you want to use.
 
+import sys
 import chess
 import chess.engine
 import bot
@@ -8,18 +11,23 @@ STOCKFISH_PATH = "/home/matt/software/stockfish/stockfish_20090216_x64_bmi2"
 
 
 def setup():
+    """Sets up the game
+
+    returns:
+        chessboard handle
+    """
     board = chess.Board()
-    demo_game(board)
-    print("Game Over")
+    return board
 
 
-def demo_game(board):
-    """Run a test game with the stockfish engine playing against itself
+def demo_game(board, engine_path):
+    """Run a test game with a chess engine playing against itself
 
     args:
         board: board to play on
+        engine_path: path to engine to be used
     """
-    engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
+    engine = chess.engine.SimpleEngine.popen_uci(engine_path)
 
     while not board.is_game_over():
         bot.bot_fighting_fish(board, engine, 0.1)
@@ -30,5 +38,10 @@ def demo_game(board):
 
 
 if __name__ == "__main__":
-    setup()
-    print(0)
+    engine_path = sys.argv[1]
+
+    if not engine_path:
+        NotImplemented
+
+    board = setup()
+    demo_game(board, engine_path)
