f435c313d6c4dd9f9a59eedbd1e34a785da7806e
Matthew Blake <mblak0625@gmail.com>
2021-02-06 11:24:19 -0500

    added some test code for chess, updated gitignore (venvs are dumb)

--- duel.py
+++ duel.py
@@ -1,2 +1,13 @@
 # duel.py - run a simulation of a chess tournaments and output the results
 
+import chess
+
+
+def setup():
+    board = chess.Board()
+    print(board)
+
+
+if __name__ == "__main__":
+    setup()
+    print(0)
