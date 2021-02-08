8a6e61541ecc38611df5b8fb0e47f6946d9fc960
Matthew Blake <mblak0625@gmail.com>
2021-02-08 10:18:28 -0500

    bagan working on bot.bot_timeout_search()

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import chess
 import random
+from queue import PriorityQueue
 
 
 def bot_random(board):
@@ -25,6 +26,28 @@ def bot_engine(board, engine, timelimit):
     """
     result = engine.play(board, chess.engine.Limit(timelimit))
     board.push(result.move)
+
+
+def bot_timeout_search(board, timelimit):
+    """Search for a move until the time limit runs out
+
+    args:
+        board: chess.board handle
+        timelimit: how much time the engine has to make a move (in seconds)
+    """
+    move_queue = PriorityQueue()
+
+    # TODO:
+    #   * Set up a timeout exception
+    #   * Loop until timeout
+    #   * Set up a priority queue with the best moves
+    #   * Factors include, piece advantage, position, castling
+    #   * On timeout, return the highest priority move
+    #
+    # For the timing, signal seems to be the correct package,
+    # but it has a different api if you're using Windows, so this function
+    # should take care of that as well.
+    board.push(move_queue.get())
 
 
 if __name__ == "__main__":
